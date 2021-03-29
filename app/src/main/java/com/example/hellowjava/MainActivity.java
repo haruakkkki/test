1890c81cd4d060061566b4c3b4f1795dfdd94756
haruakkkki <1688haruaki@gmail.com>
2021-03-29 10:27:39 -0700

    Initial commit

---
+++ app/src/main/java/com/example/hellowjava/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.hellowjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+package day5;
+
+public class Sample503 {
+
+    public static void main(String[] args) {
+        int n[] = { 5,4,3,2,1 };
+        String s[] = {"ABC","DEF","GHI" };
+        int i;
+        //  整数型配列変数nの成分表示
+        for(i = 0; i < n.length; i++){
+            System.out.print(n[i]+" ");
+        }
+        System.out.println();
+        //  文字列型配列変数nの成分表示
+        for(i = 0; i < s.length; i++){
+            System.out.print(s[i]+" ");
+        }
+        System.out.println();
+    }
+}
